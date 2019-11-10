3ac7df9b49dfbbba82ebcdc14ce3706dd48877ef
Luca Donato <donluca75@gmail.com>
2019-11-10 10:42:15 +0100

    bug correction

---
+++ MovieArchive/MovieArchive/Models/ApiKey.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieArchive
+{
+    static class ApiKey
+    {
+        //Personal api key repository
+        public static string OMDApikey = "4cfc85be";
+        public static string tmdbkeyV3 = "c039829963e62abf79aba835cebf13c7";
+        public static string tmdbkeyV4 = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMDM5ODI5OTYzZTYyYWJmNzlhYmE4MzVjZWJmMTNjNyIsInN1YiI6IjRjNWQ1ODk4N2I5YWExNTFmMjAwMDljNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.o9Qw4eVQvx_eFHzc4kkqBMVV8MMIaAvbPLWjgLtGDTg";
+        public const string DropBoxClientID = "4bfwv70jq1tb61y";
+    }
+}
